9596611af1580d1cc58b5bad48b2461d5d7cac31
alecgreene <acgreene@umich.edu>
2022-11-04 16:29:38 -0400

    Updated js gui.

--- web_gui/public/index.js
+++ web_gui/public/index.js
@@ -1,5 +1,9 @@
-// Get the web page body
 const gui = document.body;
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
+const overlay = document.getElementById('overlay');
+const title = document.getElementById('title');
+const toolbar = document.getElementById('toolbar');
 
 // template fetch function
 let doFetch = async function fetcher(route, method, body, err) {
@@ -42,4 +46,76 @@ function mouseCoords(event) {
     doFetch('/mouse', 'POST', mouse, 'Unable to send mouse coords')
 }
 
-gui.addEventListener("mousemove", mouseCoords, false);
+function sizeCanvas() {
+    canvas.width = window.innerWidth;
+    canvas.height = gui.offsetHeight - toolbar.offsetHeight;
+}
+
+sizeCanvas();
+
+canvas.addEventListener("mousemove", mouseCoords, false);
+
+// Maintain drawings on screen when window is resized
+window.addEventListener('resize', () => {
+    sizeCanvas();
+
+    if (canvas.getContext) {
+        // const ctx = canvas.getContext('2d');
+        for (let i = 0; i < clickPoints.length; i++) {
+            ctx.beginPath();
+            ctx.arc(clickPoints[i].x, clickPoints[i].y, 30, 0, 2 * Math.PI);
+            ctx.fill();
+        }
+    }
+})
+
+let clickPoints = [];
+
+function draw() {
+    ctx.beginPath();
+    if (clickPoints.length > 1) {
+        ctx.moveTo(clickPoints[0].x, clickPoints[0].y);
+
+        // for each click point make a shape
+        for (let i = 1; i < clickPoints.length; i++) {
+            ctx.lineTo(clickPoints[i].x, clickPoints[i].y);
+            ctx.fill()
+        }
+    }
+}
+
+canvas.addEventListener("click", (event) => {
+    let click = {
+        x: event.clientX,
+        y: event.clientY - toolbar.offsetHeight
+    };
+    clickPoints.push(click);
+
+    if (canvas.getContext) {
+        // append div to screen that looks like a circle at click point
+        let effect = document.createElement('div');
+        effect.className = 'effect';
+        effect.style.left = String(click.x - 25) + 'px';
+        effect.style.top = String(click.y - 25) + 'px';
+
+        let fxSelector = document.getElementById('fxSelector');
+        let fxName = fxSelector.options[fxSelector.selectedIndex].text; 
+
+        let effectText = document.createElement('span');
+        effectText.innerHTML = fxName;
+
+        effect.appendChild(effectText);
+        overlay.appendChild(effect);
+        
+        // initialize drawing canvas
+        // const ctx = canvas.getContext('2d');
+        // ctx.beginPath();
+
+
+        // ctx.arc(click.x, click.y, 30, 0, 2 * Math.PI);
+        // ctx.fill();
+
+        draw();
+        
+    }
+})
