791b02c54f383480c17f2cc5005d377196f262d5
marcotgp <78632454+marcotgp@users.noreply.github.com>
2022-11-08 17:58:21 -0500

    Update distortion_test.py

--- distortion_test.py
+++ distortion_test.py
@@ -5,37 +5,38 @@ from scipy.io import wavfile
 
 
 samplerate, data = wavfile.read('mixed_sound.wav')
-
-print("Argmax is = ",np.argmax(data),"\n")
-max_ = np.argmax(data)
+max_ = max(abs(data))
 data = data/max_
+new_data = []
+
+for x in range (len(data)):
+    if abs(data[x])<(1/3):
+        new_val = data[x]*2
+        new_data.append(new_val*max_)
+    elif abs(data[x])>1/3 and abs(data[x])<2/3:
+        if np.sign(data[x]) > 0:
+            new_val = (3-((2-3*data[x])**2))/3
+            new_data.append(new_val*max_)
+        elif np.sign(data[x]) < 0:
+            new_val = -(3-((2+3*data[x])**2))/3
+            new_data.append(new_val*max_)
+    else:
+        new_val = np.sign(data[x])*1
+        new_data.append(new_val*max_)
+
+data = data*max_
 plt.plot(data)
 plt.title("original data")
 plt.xlabel("Sample index")
 plt.ylabel("Amplitude")
 plt.show()
 
-saw = -(data%2)+1
-saw = saw*max_
-plt.plot(saw)
-plt.title("sawtooth data")
+plt.plot(new_data)
+plt.title("new data")
 plt.xlabel("Sample index")
 plt.ylabel("Amplitude")
 plt.show()
 
-#data = np.abs((data)%2-1)*2-1
-data = np.where((data)%2<1,-1,1)
-data = data*max_
 
-
-plt.plot(data)
-plt.title("distorted data")
-plt.xlabel("Sample index")
-plt.ylabel("Amplitude")
-plt.show()
-
-sd.play(np.int16(saw), samplerate)
-wavfile.write("file.wav", samplerate, np.int16(saw))
-
-
-#%%
+sd.play(np.int16(new_data), samplerate)
+wavfile.write("file.wav", samplerate, np.int16(new_data))
