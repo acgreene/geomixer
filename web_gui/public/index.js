eedcd84fc02510e4546c828f62133168111e5530
alecgreene <acgreene@umich.edu>
2022-11-12 16:51:33 -0500

    Can add and remove fx, shapes draw dynamically, created detection algorithm to detect when mouse is in shape.

--- web_gui/public/index.js
+++ web_gui/public/index.js
@@ -1,121 +1,66 @@
+///// STATIC DOM OBJECTS /////
 const gui = document.body;
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 const overlay = document.getElementById('overlay');
 const title = document.getElementById('title');
 const toolbar = document.getElementById('toolbar');
+const deleteEffectsButton = document.getElementById('delete-fx-button');
+const fxSelector = document.getElementById('fxSelector');
 
-// template fetch function
-let doFetch = async function fetcher(route, method, body, err) {
-    let args = {
-        method: method,
-        headers: {
-            'Content-Type': 'application/json'
-        }
-    }
+///// GEOMIXER OBJECTS /////
+let mouse = new Mouse();
+let shape = new Shape(canvas);
 
-    if (body != null) {
-        args.body = JSON.stringify(body)
+///// INTERACTIONS WITH SERVER /////
+function postMouseCoords(event) {
+    let yOffset = toolbar.offsetHeight;
+    mouse.updatePos(event, yOffset);
+
+    if (mouse.isInsideShape(shape) == false) {
+        ctx.fillStyle = 'black';
+        ctx.fill();
+        return;
     }
-    
-    return fetch(route, args)
-    .then(res => {
-        if (res.ok) {
-            return res.json()
-        }
-        else {
-            throw new Error(err)
-        }
-    }).then(res => {
-        return res
-    })
+    else {
+        ctx.fillStyle = 'red';
+        ctx.fill();
+        // TO DO: instead of sending mouse coords, send mix amounts
+        doFetch('/mouse', 'POST', mouse.coordinates, 'Unable to send mouse coords')
+    }
 }
+canvas.addEventListener("mousemove", postMouseCoords, false);
 
-// Get the mouse coordinates and send them to serial monitor
-function mouseCoords(event) {
-    let mouseX = event.pageX;
-    let mouseY = event.pageY;
-
-    let mouse = {
-        x: mouseX,
-        y: mouseY
-    }
-
-    // console.log(mouse)
-
-    doFetch('/mouse', 'POST', mouse, 'Unable to send mouse coords')
-}
-
+////// CANVAS FUNCTIONALITIY /////
 function sizeCanvas() {
     canvas.width = window.innerWidth;
     canvas.height = gui.offsetHeight - toolbar.offsetHeight;
 }
-
 sizeCanvas();
 
-canvas.addEventListener("mousemove", mouseCoords, false);
+// create point on canvas
+canvas.addEventListener("click", (event) => {
+    // obtain click coordinates
+    let xClick = event.clientX;
+    let yClick = event.clientY - toolbar.offsetHeight;
 
-// Maintain drawings on screen when window is resized
-window.addEventListener('resize', () => {
-    sizeCanvas();
+    // create effect div
+    let effect = document.createElement('div');
+    let fxName = fxSelector.options[fxSelector.selectedIndex].text; 
 
-    if (canvas.getContext) {
-        // const ctx = canvas.getContext('2d');
-        for (let i = 0; i < clickPoints.length; i++) {
-            ctx.beginPath();
-            ctx.arc(clickPoints[i].x, clickPoints[i].y, 30, 0, 2 * Math.PI);
-            ctx.fill();
-        }
-    }
+    // create point using coordinates and div
+    shape.createPoint(xClick, yClick, effect, overlay, fxName);
+    
+    // update the shape properties
+    shape.update();
 })
 
-let clickPoints = [];
-
-function draw() {
-    ctx.beginPath();
-    if (clickPoints.length > 1) {
-        ctx.moveTo(clickPoints[0].x, clickPoints[0].y);
-
-        // for each click point make a shape
-        for (let i = 1; i < clickPoints.length; i++) {
-            ctx.lineTo(clickPoints[i].x, clickPoints[i].y);
-            ctx.fill()
+// delete point on canvas from delete fx button
+deleteEffectsButton.addEventListener("click", (event) => {
+    for (let j = 0; j < shape.points.length; j++) {
+        if (shape.deletePoint(shape.points[j], j, overlay)) {
+            j--;
         }
     }
-}
-
-canvas.addEventListener("click", (event) => {
-    let click = {
-        x: event.clientX,
-        y: event.clientY - toolbar.offsetHeight
-    };
-    clickPoints.push(click);
-
-    if (canvas.getContext) {
-        // append div to screen that looks like a circle at click point
-        let effect = document.createElement('div');
-        effect.className = 'effect';
-        effect.style.left = String(click.x - 25) + 'px';
-        effect.style.top = String(click.y - 25) + 'px';
-
-        let fxSelector = document.getElementById('fxSelector');
-        let fxName = fxSelector.options[fxSelector.selectedIndex].text; 
-
-        let effectText = document.createElement('span');
-        effectText.innerHTML = fxName;
-
-        effect.appendChild(effectText);
-        overlay.appendChild(effect);
-        
-        // initialize drawing canvas
-        // const ctx = canvas.getContext('2d');
-        // ctx.beginPath();
-
-
-        // ctx.arc(click.x, click.y, 30, 0, 2 * Math.PI);
-        // ctx.fill();
-
-        draw();
-        
-    }
+    shape.update();
 })
