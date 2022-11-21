88d0817a813ab9e0713d5c1b09bee34cb26d951a
marcotgp <78632454+marcotgp@users.noreply.github.com>
2022-11-21 13:44:25 -0500

    Add files via upload

---
+++ AudioEffectDistortion2.h
@@ -0,0 +1,20 @@
+#ifndef effect_distortion2_h_
+#define effect_distortion2_h_
+#include "Arduino.h"
+#include "AudioStream.h"
+
+class AudioEffectDistortion2 : public AudioStream
+{
+public:
+        AudioEffectDistortion2() : AudioStream(1, inputQueueArray)
+        {
+        
+                
+        }
+        virtual void update(void);
+private:
+        int max = 32768
+        audio_block_t *inputQueueArray[1];
+        
+};
+#endif
