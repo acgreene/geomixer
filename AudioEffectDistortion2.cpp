8a0af5aaa08d6fcd93be99f0d1d1a491141f41f9
marcotgp <78632454+marcotgp@users.noreply.github.com>
2022-11-21 15:22:47 -0500

    Update AudioEffectDistortion2.cpp

--- AudioEffectDistortion2.cpp
+++ AudioEffectDistortion2.cpp
@@ -20,6 +20,10 @@ void AudioEffectDistortion2::update(void) {
     Serial.println("firstloop");
     Serial.println("after first loop");
     
+/*
+TO DO: If necessary scale input and output depending on the desired sound
+*/
+
 
     for (int i = 0; i < AUDIO_BLOCK_SAMPLES; i++)
     {
@@ -27,12 +31,13 @@ void AudioEffectDistortion2::update(void) {
         if (abs((*bp)) > ((1/3)*float(AudioEffectDistortion2.max_)) && abs(*bp) < ((2/3)*float(AudioEffectDistortion2.max_))){
             *bp = (*bp/abs(*bp))*float(AudioEffectDistortion2.max_)*((3-pow((2-(*bp/abs(*bp))*(3.0*float(*bp)/float(AudioEffectDistortion2.max_))),2))/6);
         }
-
+        //This if statement above creates the curvature for the clipping when the signal is between 1/3 and 2/3 of the max
         
         
         else if (abs(*bp) > ((2/3)*float(AudioEffectDistortion2.max_))){
             *bp = (*bp/abs(*bp))*(1/2)*float(AudioEffectDistortion2.max_);
         }
+        //This if statement above flattens the signal when it is beyond 2/3 of the max
         
         bp++;
       }
