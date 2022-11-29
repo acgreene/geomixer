cae24c26598011852047b53b59ec04232fdb53bc
ocsean23 <55564105+ocsean23@users.noreply.github.com>
2022-11-29 12:37:15 -0500

    Add files via upload

--- AudioEffectChorus2.cpp
+++ AudioEffectChorus2.cpp
@@ -6,212 +6,88 @@
 //attempting to write 29 ms delay chorus
 void AudioEffectChorus2::changeNum(int numVoices)
 {
-  Serial.println("changeNum");
+  //Serial.println("changeNum");
   num_voices = numVoices;
 }
 
+
 void AudioEffectChorus2::update(void) {
   
-  Serial.println("update");
+  //Serial.println("update");
   audio_block_t *block;
   block = receiveWritable(0);
-  if(block)
-  {
-    short *bp = block->data;
-    Serial.println("firstloop");
-    Serial.println("after first loop");
-    Serial.println(num_voices);
-    if(num_voices == 0)
-    {
-      short* cpy = bp;
-      for(int i = AUDIO_BLOCK_SAMPLES; i<31*AUDIO_BLOCK_SAMPLES;i++ )
-      {
-        if(i<11*AUDIO_BLOCK_SAMPLES)
-        {
-          voice1[i-AUDIO_BLOCK_SAMPLES] = voice1[i];
-        }
-        if(i<21*AUDIO_BLOCK_SAMPLES)
-        {
-          voice2[i-AUDIO_BLOCK_SAMPLES]=voice2[i];
-        }
-        voice3[i-AUDIO_BLOCK_SAMPLES]=voice3[i];
-        
-      }
-      for(int i = 10*AUDIO_BLOCK_SAMPLES; i<11*AUDIO_BLOCK_SAMPLES; i++)
-      {
-        voice1[i] = *cpy++;
-      }
-      cpy = bp;
-      for(int i = 20*AUDIO_BLOCK_SAMPLES; i<21*AUDIO_BLOCK_SAMPLES; i++)
-      {
-        voice2[i] = *cpy++;
-      }
-      cpy = bp;
-      for(int i = 30*AUDIO_BLOCK_SAMPLES; i<31*AUDIO_BLOCK_SAMPLES; i++)
-      {
-        voice3[i] = *cpy++;
-      }
-      cpy = bp;
-      for(int i = 40*AUDIO_BLOCK_SAMPLES; i<41*AUDIO_BLOCK_SAMPLES; i++)
-      {
-        voice4[i] = *cpy++;
-      }
-      Serial.println("0");
-      transmit(block,0);
-      Serial.println("after trasnmit");
-      release(block);
-      Serial.println("after0");
-    }
-    else if(num_voices == 1)
-    {
-      Serial.println("one voice");
-      short cpy1[AUDIO_BLOCK_SAMPLES];
-      for(int i = 0; i <AUDIO_BLOCK_SAMPLES; i++)
-      {
-        cpy1[i]=voice1[i];
-      }
-      short* cpy = bp;
-      for(int i = AUDIO_BLOCK_SAMPLES; i<31*AUDIO_BLOCK_SAMPLES;i++ )
-      {
-        if(i<11*AUDIO_BLOCK_SAMPLES)
-        {
-          voice1[i-AUDIO_BLOCK_SAMPLES] = voice1[i];
-        }
-        if(i<21*AUDIO_BLOCK_SAMPLES)
-        {
-          voice2[i-AUDIO_BLOCK_SAMPLES]=voice2[i];
-        }
-        voice3[i-AUDIO_BLOCK_SAMPLES]=voice3[i];
-        
-      }
-      for(int i = 10*AUDIO_BLOCK_SAMPLES; i<11*AUDIO_BLOCK_SAMPLES; i++)
-      {
-        voice1[i] = *cpy++;
-      }
-      cpy = bp;
-      for(int i = 20*AUDIO_BLOCK_SAMPLES; i<21*AUDIO_BLOCK_SAMPLES; i++)
-      {
-        voice2[i] = *cpy++;
-      }
-      cpy = bp;
-      for(int i = 30*AUDIO_BLOCK_SAMPLES; i<31*AUDIO_BLOCK_SAMPLES; i++)
-      {
-        voice3[i] = *cpy++;
-      }
-      cpy = bp;
-      for(int i = 40*AUDIO_BLOCK_SAMPLES; i<41*AUDIO_BLOCK_SAMPLES; i++)
-      {
-        voice4[i] = *cpy++;
-      }
-      for(int i = 0; i<AUDIO_BLOCK_SAMPLES; i++)
-      {
-        bp[i] = (bp[i] +cpy1[i])/2; 
-      }
-      transmit(block,0);
-      release(block);
-    }
-    else if(num_voices == 2)
-    {
-      short cpy1[AUDIO_BLOCK_SAMPLES];
-      short cpy2[AUDIO_BLOCK_SAMPLES];
-      for(int i = 0; i <AUDIO_BLOCK_SAMPLES; i++)
-      {
-        cpy1[i]=voice1[i];
-        cpy2[i] = voice2[i];
-      }
-      short* cpy = bp;
-      for(int i = AUDIO_BLOCK_SAMPLES; i<31*AUDIO_BLOCK_SAMPLES;i++ )
-      {
-        if(i<11*AUDIO_BLOCK_SAMPLES)
-        {
-          voice1[i-AUDIO_BLOCK_SAMPLES] = voice1[i];
-        }
-        if(i<21*AUDIO_BLOCK_SAMPLES)
-        {
-          voice2[i-AUDIO_BLOCK_SAMPLES]=voice2[i];
-        }
-        voice3[i-AUDIO_BLOCK_SAMPLES]=voice3[i];
-        
-      }
-      for(int i = 10*AUDIO_BLOCK_SAMPLES; i<11*AUDIO_BLOCK_SAMPLES; i++)
-      {
-        voice1[i] = *cpy++;
-      }
-      cpy = bp;
-      for(int i = 20*AUDIO_BLOCK_SAMPLES; i<21*AUDIO_BLOCK_SAMPLES; i++)
-      {
-        voice2[i] = *cpy++;
-      }
-      cpy = bp;
-      for(int i = 30*AUDIO_BLOCK_SAMPLES; i<31*AUDIO_BLOCK_SAMPLES; i++)
-      {
-        voice3[i] = *cpy++;
-      }
-      cpy = bp;
-      for(int i = 40*AUDIO_BLOCK_SAMPLES; i<41*AUDIO_BLOCK_SAMPLES; i++)
-      {
-        voice4[i] = *cpy++;
-      }
-      for(int i = 0; i<AUDIO_BLOCK_SAMPLES; i++)
-      {
-        bp[i] = (bp[i] +cpy1[i]+cpy2[i])/3; 
-      }
-      transmit(block,0);
-      release(block);
-    }
-    else if(num_voices == 3)
-    {
-      short cpy1[AUDIO_BLOCK_SAMPLES];
-      short cpy2[AUDIO_BLOCK_SAMPLES];
-      short cpy3[AUDIO_BLOCK_SAMPLES];
-      for(int i = 0; i <AUDIO_BLOCK_SAMPLES; i++)
-      {
-        cpy1[i]=voice1[i];
-        cpy2[i] = voice2[i];
-        cpy3[i] = voice3[i];
-      }
-      short* cpy = bp;
-      for(int i = AUDIO_BLOCK_SAMPLES; i<31*AUDIO_BLOCK_SAMPLES;i++ )
-      {
-        if(i<11*AUDIO_BLOCK_SAMPLES)
-        {
-          voice1[i-AUDIO_BLOCK_SAMPLES] = voice1[i];
-        }
-        if(i<21*AUDIO_BLOCK_SAMPLES)
-        {
-          voice2[i-AUDIO_BLOCK_SAMPLES]=voice2[i];
-        }
-        voice3[i-AUDIO_BLOCK_SAMPLES]=voice3[i];
-        
-      }
-      for(int i = 10*AUDIO_BLOCK_SAMPLES; i<11*AUDIO_BLOCK_SAMPLES; i++)
-      {
-        voice1[i] = *cpy++;
-      }
-      cpy = bp;
-      for(int i = 20*AUDIO_BLOCK_SAMPLES; i<21*AUDIO_BLOCK_SAMPLES; i++)
-      {
-        voice2[i] = *cpy++;
-      }
-      cpy = bp;
-      for(int i = 30*AUDIO_BLOCK_SAMPLES; i<31*AUDIO_BLOCK_SAMPLES; i++)
-      {
-        voice3[i] = *cpy++;
-      }
-      cpy = bp;
-      for(int i = 40*AUDIO_BLOCK_SAMPLES; i<41*AUDIO_BLOCK_SAMPLES; i++)
-      {
-        voice4[i] = *cpy++;
-      }
-      for(int i = 0; i<AUDIO_BLOCK_SAMPLES; i++)
-      {
-        bp[i] = (bp[i] +cpy1[i]+cpy2[i]+cpy3[i])/4; 
-      }
-      transmit(block,0);
-      release(block);
-    }
-    
-    Serial.println("endloop");
-  }
+  //new code with one array, remember to change private variables when switching
   
+    if(block)
+    {
+      short *bp = block->data;
+      //Serial.println("firstloop");
+      //Serial.println("after first loop");
+      //Serial.println(num_voices);
+      if(num_voices == 0)
+      {
+        short* cpy = bp;
+        buffer.insertBlock(cpy);
+        
+        
+        transmit(block,0);
+        //Serial.println("after trasnmit");
+        release(block);
+        //Serial.println("after0");
+      }
+      else if(num_voices == 1)
+      {
+        //Serial.println("one voice");
+        
+        short* cpy = bp;
+        buffer.insertBlock(cpy);
+        
+        
+        for(int i = 20*AUDIO_BLOCK_SAMPLES; i<21*AUDIO_BLOCK_SAMPLES; i++)
+        {
+          short current1 = buffer.getValue(i);
+          bp[i-20*AUDIO_BLOCK_SAMPLES] = (bp[i-20*AUDIO_BLOCK_SAMPLES] +current1)/2; 
+        }
+        transmit(block,0);
+        release(block);
+      }
+      else if(num_voices == 2)
+      {
+        
+        short* cpy = bp;
+        buffer.insertBlock(cpy);
+        
+        
+        for(int i = 20*AUDIO_BLOCK_SAMPLES; i<21*AUDIO_BLOCK_SAMPLES; i++)
+        {
+          short current1 = buffer.getValue(i);
+          short current2 = buffer.getValue((i-10*AUDIO_BLOCK_SAMPLES));
+          bp[i-20*AUDIO_BLOCK_SAMPLES] = (bp[i-20*AUDIO_BLOCK_SAMPLES] +current1+current2)/3; 
+        }
+        transmit(block,0);
+        release(block);
+      }
+      else if(num_voices == 3)
+      {
+        
+        short* cpy = bp;
+        buffer.insertBlock(cpy);
+        
+        for(int i = 20*AUDIO_BLOCK_SAMPLES; i<21*AUDIO_BLOCK_SAMPLES; i++)
+        {
+          short current1 = buffer.getValue(i);
+          short current2 = buffer.getValue((i-10*AUDIO_BLOCK_SAMPLES));
+          short current3 = buffer.getValue((i-20*AUDIO_BLOCK_SAMPLES));
+          short toAdd = (bp[i-20*AUDIO_BLOCK_SAMPLES] + current1)/4;
+          toAdd += current2/4;
+          toAdd += current3/4;
+          bp[i-20*AUDIO_BLOCK_SAMPLES] = toAdd;
+          //bp[i-20*AUDIO_BLOCK_SAMPLES] = (bp[i-20*AUDIO_BLOCK_SAMPLES] + current1)/4 + (current2 + current3)/4; 
+        }
+        transmit(block,0);
+        release(block);
+      }
+      
+      //Serial.println("endloop");
+    }
 }
---
+++ circular_buffer.cpp
@@ -0,0 +1,69 @@
+#include "circular_buffer.h"
+#include <Arduino.h>
+
+
+short circular_buffer::getValue(int indexDesired){
+    //Serial.println((circ_idx + indexDesired) %capacity);
+    if((circ_idx + indexDesired) < capacity)
+    {
+        return c_array[(circ_idx + indexDesired)];
+    }
+    else if((circ_idx + indexDesired) >= capacity)
+    {
+        return c_array[(circ_idx + indexDesired)-capacity];
+    }
+    
+    //return c_array[(circ_idx + indexDesired) % capacity];
+}
+void circular_buffer::insertBlock(short* bp)
+{
+    short* values = bp;
+    for(int i = circ_idx; i < (circ_idx + 128); i++)
+    {
+        if(i < capacity)
+        {
+            c_array[i] = *values;
+            values++;
+        }
+        else if(i >= capacity)
+        {
+            c_array[i - capacity] = *values;
+            values++;
+        }
+        /*c_array[i % capacity] = *values;
+        values++;*/
+    }
+    circ_idx += 128;
+    if(circ_idx >= capacity)
+    {
+        circ_idx -= capacity;
+    }
+    /*circ_idx += 128;
+    circ_idx = circ_idx % capacity;*/
+    
+}
+
+/* void circular_buffer::phaserChangeDelay(int diff)
+{
+    if(capacity + diff > MAX_ARRAY_SIZE)
+    {
+        Serial.println("Change Delay Too Big");
+    }
+    //orig - new, positive implies less delay
+    if(diff >0)
+    {
+        capacity -= diff;
+        circ_idx += diff;
+        circ_idx = circ_idx % capacity;
+    }
+    else if(diff < 0)
+    {
+        capacity -= diff;
+        int new_circ_idx = circ_idx + diff;
+        for(int i = new_circ_idx; (i % capacity) < circ_idx; ++i)
+        {
+            c_array[i % capacity] = 0;
+        }
+        circ_idx = new_circ_idx % capacity;
+    }
+} */
