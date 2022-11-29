3359ef4d80733d72ce3a3e1510a2caa761d8f399
marcotgp <78632454+marcotgp@users.noreply.github.com>
2022-11-29 13:14:44 -0500

    Update AudioEffectDistortion2.cpp

--- Audio/AudioEffectDistortion2.cpp
+++ Audio/AudioEffectDistortion2.cpp
@@ -4,12 +4,13 @@
 
 //update is called every 128 samples/2.9 ms
 //attempting to write 29 ms delay chorus
-void AudioEffectDistortion2::changeNum()
-{
+//void AudioEffectDistortion2::changeNum()
+//{
   
-}
+//}
 
 void AudioEffectDistortion2::update(void) {
+int start = micros();
   
   Serial.println("update");
   audio_block_t *block;
@@ -19,25 +20,31 @@ void AudioEffectDistortion2::update(void) {
     short *bp = block->data;
     Serial.println("firstloop");
     Serial.println("after first loop");
-    bp = bp/AudioEffectDistortion2.max_
+
+    //sign = 0x8000 & *bp < 0 ? -1 : 1
+    // if necessary replace *bp/abs(*bp)
     
+/*
+TO DO: If necessary scale input and output depending on the desired sound
+*/
+
 
     for (int i = 0; i < AUDIO_BLOCK_SAMPLES; i++)
     {
+        if (*bp != 0){
+        if (abs(*bp) > ((1/3)*(float)(AudioEffectDistortion2::max_)) && abs(*bp) < ((2/3)*(float)(AudioEffectDistortion2::max_))){
+            *bp = (*bp/abs(*bp))*(float)(AudioEffectDistortion2::max_)*((3-pow((2-(*bp/abs(*bp))*(3.0*(float)(*bp)/(float)(AudioEffectDistortion2::max_))),2))/6);
+        }
+        //This if statement above creates the curvature for the clipping when the signal is between 1/3 and 2/3 of the max
         
-
-        if (bp > 1/3 && bp < 2/3){
-            bp = (3-((2-3*pow(bp,2)))/6);
+        
+        else if (abs(*bp) > ((2/3)*(float)(AudioEffectDistortion2::max_))){
+            *bp = (*bp/abs(*bp))*(1/2)*(float)(AudioEffectDistortion2::max_);
         }
-        else if (bp < (-1/3) && bp > (-2/3)){
-            bp = (-1)*(3-((2+3*pow(bp,2)))/6);
+        //This if statement above flattens the signal when it is beyond 2/3 of the max
         }
-        else if (bp > 2/3){
-            bp = 1/2;
-        }
-        else if (bp < -2/3){
-            bp = -1/2;
-        }
+        
+        bp++;
       }
     
       Serial.println("0");
@@ -46,4 +53,8 @@ void AudioEffectDistortion2::update(void) {
       release(block);
       Serial.println("after0");
     }
+int end = micros();
+int duration = end - start;
+Serial.println(duration);
+
 }
