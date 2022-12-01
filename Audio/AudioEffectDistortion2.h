fd82eff1d23969af68e6ecb75e7b36956f820b9d
marcotgp <78632454+marcotgp@users.noreply.github.com>
2022-12-01 14:51:11 -0500

    Update AudioEffectDistortion2.h

--- Audio/AudioEffectDistortion2.h
+++ Audio/AudioEffectDistortion2.h
@@ -8,12 +8,12 @@ class AudioEffectDistortion2 : public AudioStream
 public:
         AudioEffectDistortion2() : AudioStream(1, inputQueueArray)
         {
-        max_ = 3277;
+        output_scalar = 3277;
                 
         }
         virtual void update(void);
 private:
-        int32_t max_;
+        int32_t output_scalar;
        
         audio_block_t *inputQueueArray[1];
         
