d9687316450a33f93ebc6c8bff98bc43a3f738e4
lokibler <lokibler@umich.edu>
2022-12-02 15:41:05 -0500

    update arduino control

--- arduino/control/control.ino
+++ arduino/control/control.ino
@@ -31,8 +31,14 @@ AudioControlSGTL5000 audioShield;
 
 enum effects1{DISTORTION, CHORUS, PHASER, CLEAN1};
 enum effects2{STEREO, CLEAN2};
-String rc, c1, c2, c3, c4, c5;
-float g1, g2, g3, g4, g5;
+String rc, s;
+char tags[3];
+float in_mixes[3];
+float mixes[5];
+int i;
+float val;
+enum locs{D, C, P, S, U};
+
 
 
 void setup() {
@@ -54,55 +60,104 @@ void setup() {
   chorus.changeNum(2);
   phaser.begin(220);
   stereo.begin();
-  stereo.mix(0.5);
+  stereo.mix(0);
 }
 
+
+
+void run_distortion(float f) {
+  if(f >= 0 && f <= 1){
+      mixer1.gain(DISTORTION, f);
+      Serial.print("Updated distortion to ");
+      Serial.println(f);
+  }
+}
+
+void run_chorus(float f) {
+  if(f >= 0 && f <= 1){
+      mixer1.gain(CHORUS, f);
+      //adaptive parameter: num voices?
+      Serial.print("Updated chorus to ");
+      Serial.println(f);
+    }
+}
+
+void run_phaser(float f) {
+  if(f >= 0 && f <= 1){
+      mixer1.gain(PHASER, f);
+      //phaser.changeDelay((f * 1103 + 220)); //adaptive parameter: 220 to 1323?
+      Serial.print("Updated phaser to ");
+      Serial.println(f);
+    }
+}
+
+void run_stereo(float f) {
+  if(f >= 0 && f <= 1){
+      stereo.mix(f); //change amount of stereo spread
+      Serial.print("Updated stereo to ");
+      Serial.println(f);
+      if(mixes[U] == 0 && f > 0){
+        mixer1.gain(CLEAN1, f);
+        Serial.println("(and clean as well) ");
+      }
+    }
+}
+
+void run_clean(float f) {
+  if(f >= 0 && f <= 1){
+      if(mixes[S] > 0){
+        val = f + mixes[S];
+        mixer1.gain(CLEAN1, val);
+        Serial.print("Updated clean to ");
+        Serial.println(val);
+      }else{
+        mixer1.gain(CLEAN1, f);
+        Serial.print("Updated clean to ");
+        Serial.println(f);
+      }
+    }
+}
+
+
+
 void loop() {
-  
   if(Serial.available() > 0){
-    
-    //type in form <0.00,0.00,0.00,0.00,0.00>
-    //note: <distortion, chorus, phasor, stereo, clean>
     rc = Serial.readString();
     Serial.println(rc);
-    
-    c1 = rc.substring(1,5);
-    g1 = c1.toFloat();
-    c2 = rc.substring(6,10);
-    g2 = c2.toFloat();
-    c3 = rc.substring(11,15);
-    g3 = c3.toFloat();
-    c4 = rc.substring(16,20);
-    g4 = c4.toFloat();
-    c5 = rc.substring(21,25);
-    g5 = c5.toFloat();
 
-    if(g1 >= 0 && g1 <= 1){
-      mixer1.gain(DISTORTION, g1);
-      Serial.print("Updated distortion to ");
-      Serial.println(g1);
+    //distortion: D, chorus: C, phasor: P, stereo: S, clean: U
+    //c 0.000, c 0.000, c 0.000,
+    
+    tags[0] = rc.charAt(0); //parse input
+    s = rc.substring(2,7);
+    in_mixes[0] = s.toFloat();
+    
+    tags[1] = rc.charAt(9);
+    s = rc.substring(11,16);
+    in_mixes[1] = s.toFloat();
+    
+    tags[2] = rc.charAt(18);
+    s = rc.substring(20,25);
+    in_mixes[2] = s.toFloat();
+
+    for(i = 0; i < 5; i++){ //reset mixes to 0
+      mixes[i] = 0;
     }
-    if(g2 >= 0 && g2 <= 1){
-      mixer1.gain(CHORUS, g2);
-      //adaptive parameter: num voices
-      Serial.print("Updated chorus to ");
-      Serial.println(g2);
+    
+    for(i = 0; i < 3; i++){ //get mixes for each effect that are non-zero
+      switch (tags[i]) {
+        case 'D': mixes[D] = in_mixes[i]; break;
+        case 'C': mixes[C] = in_mixes[i]; break;
+        case 'P': mixes[P] = in_mixes[i]; break;
+        case 'S': mixes[S] = in_mixes[i]; break;
+        case 'U': mixes[U] = in_mixes[i]; break;
+      }
     }
-    if(g3 >= 0 && g3 <= 1){
-      mixer1.gain(PHASER, g3);
-      //phaser.changeDelay((g3 * 1103 + 220)); //adaptive parameter: 220 to 1323
-      Serial.print("Updated phaser to ");
-      Serial.println(g3);
-    }
-    if(g4 >= 0 && g4 <= 1){
-      stereo.mix(g4); //change amount of stereo spread
-      Serial.print("Updated stereo to ");
-      Serial.println(g4);
-    }
-    if(g5 >= 0 && g5 <= 1){
-      mixer1.gain(CLEAN1, g5);
-      Serial.print("Updated clean to ");
-      Serial.println(g5);
-    }
+
+    run_distortion(mixes[D]);
+    run_chorus(mixes[C]);
+    run_phaser(mixes[P]);
+    run_stereo(mixes[S]);
+    run_clean(mixes[U]);
   }
 }
