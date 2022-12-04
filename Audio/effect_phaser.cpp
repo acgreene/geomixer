fe45bfed4261483f22bd54b51bb5ce4731bcfc01
Parker Stogdill <stogdill@umich.edu>
2022-12-04 18:05:41 -0500

    New Phaser

--- Audio/effect_phaser.cpp
+++ Audio/effect_phaser.cpp
@@ -4,75 +4,71 @@
 
 void AudioEffectPhaser::update(void)
 {
-    // int start = micros();
-    // read first AUDIO_BLOCK_SIZE samples from array
-    // call function to shift all the other members of the array over by AUDIO_BLOCK_SIZE
-    // insert incoming new sample block to back of array
-    // add the samples from front of array to the new sample block
-    // transmit
-    // Serial.println("update");
+    //int start = micros();
+    //read first AUDIO_BLOCK_SIZE samples from array
+    //call function to shift all the other members of the array over by AUDIO_BLOCK_SIZE
+    //insert incoming new sample block to back of array
+    //add the samples from front of array to the new sample block
+    //transmit
+    //Serial.println("update");
     audio_block_t *block;
     short *bp;
-    if (!on)
-    {
-        // Just passthrough and add samples to queue
-        // Serial.println("off");
+    if(!on) {
+    // Just passthrough and add samples to queue
+        //Serial.println("off");
         block = receiveWritable(0);
-        if (block)
-        {
+        if(block) {
             bp = block->data;
-            // shiftLeft();
+            //shiftLeft();
             queue.insertBlock(bp);
-            transmit(block, 0);
-            // Serial.println("transmit");
+            transmit(block,0);
+            //Serial.println("transmit");
             release(block);
-            // Serial.println("release");
+            //Serial.println("release");
         }
     }
-    // Need to store the data
-    else if (on)
+    //Need to store the data
+    else if(on)
     {
         short sineOut = arm_sin_q15((q15_t)(sine_idx >> 16));
-        // Serial.println(sineOut*10);
-        int increment = (20 * 2147483648.0) / AUDIO_SAMPLE_RATE_EXACT;
-        // Serial.println(increment);
+        //Serial.println(sineOut*10);
+        int increment = (2.0 * (0.015) * (float)delayAmount * 2147483648.0)/ AUDIO_SAMPLE_RATE_EXACT;
+        //Serial.println(increment);
         sine_idx += increment;
-        // Serial.println("else");
+        //Serial.println("else");
         oscil_delay = (sineOut * OSCILLATION_AMP) >> 15;
-        // Serial.println(oscil_delay);
-        // Serial.println(delayAmount);
+        Serial.println(oscil_delay);
+        Serial.println(delayAmount);
         short cpy[AUDIO_BLOCK_SAMPLES];
-        // OSCILLATION_AMP = 40
-        // oscil_delay should be on the order of -40 to 40
-        // negative delay implies sample older samples, i.e. start 10-oscil_delay
-        // positive delay implies take newer samples, i.e. start 10+oscil_delay
+        //OSCILLATION_AMP = 40
+        //oscil_delay should be on the order of -40 to 40
+        //negative delay implies sample older samples, i.e. start 10-oscil_delay
+        //positive delay implies take newer samples, i.e. start 10+oscil_delay
         int buffer = oscil_delay + OSCILLATION_AMP;
-        // Serial.println(buffer);
+        //Serial.println(buffer);
         block = receiveWritable(0);
-        if (block)
-        {
-            bp = block->data;
-            for (int i = 0; i < AUDIO_BLOCK_SAMPLES; ++i)
-            {
-                cpy[i] = queue.getValue((MAX_DELAY - delayAmount) + buffer++);
+            if(block) {
+                bp = block->data;
+                for(int i = 0; i < AUDIO_BLOCK_SAMPLES; ++i)
+                {
+                    cpy[i] = queue.getValue((MAX_DELAY - delayAmount) + buffer++);
+                }
+                queue.insertBlock(bp);
+                for(int i = 0; i < AUDIO_BLOCK_SAMPLES; ++i)
+                {
+                    int64_t sum = *bp + (9*(cpy[i]/10));
+                    *bp = sum/2;
+                    bp++;
+                }
+                transmit(block,0);
+                release(block);
             }
-            queue.insertBlock(bp);
-            for (int i = 0; i < AUDIO_BLOCK_SAMPLES; ++i)
-            {
-                int sum = *bp + cpy[i];
-                *bp = sum/2;
-                bp++;
-            }
-            transmit(block, 0);
-            release(block);
-        }
-        if (sine_idx & 0x80000000)
-        {
+        if(sine_idx & 0x80000000) {
             sine_idx &= 0x7fffffff;
-        }
+      }
     }
-    // int end = micros();
-    // Serial.println(end - start);
+    //int end = micros();
+    //Serial.println(end - start);
 }
 
 /* void AudioEffectPhaser::shiftLeft()
@@ -95,14 +91,14 @@ void AudioEffectPhaser::insertBlock(short* bp)
     //Serial.println("end insert block");
 } */
 
-void AudioEffectPhaser::begin(short delay_length = 220)
+void AudioEffectPhaser::begin(short delay_length=220, short oscil_rate_in = 20)
 {
     this->on = true;
-    // int diff = delayAmount - delay_length;
-    // queue.phaserChangeDelay(diff);
-    if (delay_length > 1323)
-        delay_length = 1323;
+    //int diff = delayAmount - delay_length;
+    //queue.phaserChangeDelay(diff);
+    if(delay_length > 1323) delay_length = 1323;
     delayAmount = delay_length;
+    oscil_rate = oscil_rate_in;
 
     /*if(delay_length <= MAX_DELAY - AUDIO_BLOCK_SAMPLES)
     {
@@ -127,10 +123,11 @@ void AudioEffectPhaser::begin(short delay_length = 220)
                 queue[i] = 0;
             }
             this->delayAmount = delay_length;
-
+            
         }
     } */
 }
+
 void AudioEffectPhaser::setOn(bool status)
 {
     // Serial.println("Sanity check");
@@ -144,8 +141,7 @@ bool AudioEffectPhaser::getOn()
 
 void AudioEffectPhaser::changeDelay(short delay_length)
 {
-    if (delay_length > 1323)
-        delay_length = 1323;
+    if(delay_length > 1323) delay_length = 1323;
     delayAmount = delay_length;
 
     /*int diff = delay - delayAmount;
@@ -173,8 +169,8 @@ void AudioEffectPhaser::changeDelay(short delay_length)
                 queue[i] = 0;
             }
             this->delayAmount = delay;
-
-        }
+            
+        } 
     }*/
-    // Serial.println("done changeDelay");
+    //Serial.println("done changeDelay");
 }
--- Audio/effect_phaser.h
+++ Audio/effect_phaser.h
@@ -7,18 +7,19 @@
 class AudioEffectPhaser : public AudioStream
 {
 public:
-        #define OSCILLATION_AMP 128
+        #define OSCILLATION_AMP 64
         static const int MAX_DELAY = 3000 + 128 + OSCILLATION_AMP;
         //standard delay times seem to be 5ms to 30 ms, use 30 as max, 5 as default
         //converting to samples, with a 44.1 kHz sampling rate (actual rate tbd)
         //Max = 1323 samples
         //Default = 220.5 samples round to 220
-        AudioEffectPhaser() : AudioStream(1, inputQueueArray), queue(3000 + 128 + 128)
+        AudioEffectPhaser() : AudioStream(1, inputQueueArray), queue(3000 + OSCILLATION_AMP + 128)
         {
             on = false;
-            delayAmount = 220;
+            delayAmount = 300;
             sine_idx = 0;
             oscil_delay = 0;
+            oscil_rate = 15;
             //circular_buffer queue(1323 + 128 + 40);
             //circular_buffer queue(static_cast<int>(MAX_DELAY));
             //Serial.println("prememset");
@@ -29,15 +30,13 @@ public:
         //to start the effect
         //Maybe can delete, the delay line was needed for Chorus function,
         //don't really see how it helps with Phaser
-        void begin(short delay_length);
+        void begin(short delay_length, short oscil_rate_in = 20);
         //called via interrupt
         virtual void update(void);
         //easiest implementation would be in samples, may be beneficial to do it in miliseconds instead though
         void changeDelay(short delay_length);
 
         //will be useful for when effect not in triangle
-        //void turnOff();
-
         bool getOn();
 
         void setOn(bool status);
@@ -49,6 +48,7 @@ private:
         circular_buffer queue;
         unsigned int sine_idx;
         short oscil_delay;
+        short oscil_rate;
         //helper function to move the AUDIO_BLOCK_SIZE:delayAmount+AUDIO_BLOCK_SIZE samples over by AUDIO_BLOCK_SIZE
         //void shiftLeft();
         //inserts block at back of queue
